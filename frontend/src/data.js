568685387211bc465ed64bfcef66f5acd5daa57c
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-05-07 16:57:48 +0100

    initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:"1",
+            name: "Nike Slim Shirt",
+            category: "Shirts",
+            image:"/images/p1.jpg",
+            price:120,
+            Brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id:"2",
+            name: "Nike Shirt",
+            category: "Shirts",
+            image:"/images/p1.jpg",
+            price:130,
+            Brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id:"3",
+            name: "Nike Slim t",
+            category: "Shirts",
+            image:"/images/p1.jpg",
+            price:110,
+            Brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id:"4",
+            name: "Slim Shirt",
+            category: "Shirts",
+            image:"/images/p1.jpg",
+            price:13,
+            Brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id:"5",
+            name: "Nike Slim jeans",
+            category: "Shirts",
+            image:"/images/p1.jpg",
+            price:150,
+            Brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id:"6",
+            name: "Nike Slim socks",
+            category: "Shirts",
+            image:"/images/p1.jpg",
+            price:220,
+            Brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+    ]
+}
+
+export default data;
